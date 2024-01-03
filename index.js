82f9212402650a160fd2556e10d490279b723f6f
Joasper <joasper1306@gmail.com>
2024-01-02 22:30:29 -0400

    three

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const serveStatic = require("serve-static");
 const main = require("./app");
+const path = require("path");
 
 const app = express();
+
+app.use(serveStatic(path.join(__dirname, "public")));
 app.use(cors());
 
 main();
-
-app.use(express.static(__dirname + "/public"));
 
 app.listen(4001, () => {
   console.log("Servidor Montado");
